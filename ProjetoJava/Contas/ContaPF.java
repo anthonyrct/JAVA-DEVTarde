39ce4d4c58a0da9b8d62b7192cc7fea629604f1c
anthonyrct <anthonyteixaira@gmail.com>
2023-06-16 17:08:36 -0300

    16/06

--- ProjetoJava/Contas/ContaPF.java
+++ ProjetoJava/Contas/ContaPF.java
@@ -1,23 +1,5 @@
 package ProjetoJava.Contas;
 
-public class ContaPF extends Conta {
-    //atributos
-    String nCpf;
-    //métodos
-    //construtor default
-    //get and set
-
-    public String getnCpf() {
-        return nCpf;
-    }
-
-    public void setnCpf(String nCpf) {
-        this.nCpf = nCpf;
-    }
-    
-}
-package ProjetoJava.Contas;
-
 public class  ContaPF extends Conta {
      //atributos
     String nCpf;
--- ProjetoJava/Contas/ContaPJ.java
+++ ProjetoJava/Contas/ContaPJ.java
@@ -1,21 +1,4 @@
-package ProjetoJava.Contas;
 
-public class ContaPJ extends Conta{
-    //atributos
-    String nCnpj;
-    //métodos
-    //construtor default
-    //get and set
-
-    public String getnCnpj() {
-        return nCnpj;
-    }
-
-    public void setnCnpj(String nCnpj) {
-        this.nCnpj = nCnpj;
-    }
-    
-}
 package ProjetoJava.Contas;
 
 public class ContaPJ extends Conta {
