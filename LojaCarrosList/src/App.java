580ebb6e1bdb28bc5057a3662c18c50afa1f0828
anthonyrct <anthonyteixaira@gmail.com>
2023-08-14 14:26:18 -0300

    14/08/23

---
+++ LojaCarrosList/src/App.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+      List<Carros> listacarros = new ArrayList<>();
+      
+    }
+    // -criar uma classe chamada carro, colocar atibutos e metodos-
+    // manipula essa classe no app.
+    // criar uma list do tipo:List<Carros> lista carros = new ArrayList()
+    // dentro do app deve ser feito:
+    // 1. Cadrastrar Novo Carro.
+    // 2. Listar todos os Carros Cadastrados.
+    // 3. Consultar um Carro Cadastro.
+    // 4. Apagar um Carro Cadastrado.
+}
---
+++ LojaCarrosList/src/Carros.java
@@ -0,0 +1,49 @@
+public class Carros {
+    // Atributos
+    String marca;
+    String modelo;
+    String ano;
+    String cor;
+    // gets and setters
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+     //Construtor cheio
+    public Carros(String marca, String modelo, String ano, String cor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+    }
+
+
+}
