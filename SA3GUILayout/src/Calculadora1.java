4d3d2a64db5966f3435190d9d23cfbdd4a8ca210
anthonyrct <anthonyteixaira@gmail.com>
2023-09-11 16:58:16 -0300

    11/09/23

--- SA3GUILayout/src/Calculadora1.java
+++ SA3GUILayout/src/Calculadora1.java
@@ -1,14 +1,15 @@
-import javax.swing.JPanel;
-import javax.swing.JLabel;
 import javax.swing.*;
 import java.awt.*;
 
-public class Calculadora1 extends JPanel{
+public class Calculadora1 extends JPanel {
+    private JTextField idadeField;
+    private JButton calcularButton;
+    private JLabel resultadoLabel;
     public Calculadora1() {
         super();
         
-        setLayout(new GridLayout(3,2));
-        //Componentes da calculadora
+        setLayout(new GridLayout(3, 2));
+        // Componentes da calculadora
 
     }
 }
