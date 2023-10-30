47983ec20c8b1d84aef3e40f39d138bfc1812f2c
anthonyrct <anthonyteixaira@gmail.com>
2023-10-30 16:57:13 -0300

    30/10/23

---
+++ JDBCTesteDel/App.java
@@ -0,0 +1,11 @@
+package JDBCTesteDel;
+
+import java.beans.Statement;
+
+public class App {
+    public static void main(String[] args) {
+        Statement s;
+        String usuario = "postgres";
+        String senha = "postgres";
+    }
+}
